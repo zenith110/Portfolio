3bbe3591637fb6397d85607cf3036caa981473e6
zenith110 <thequake@live.com>
2022-01-30 13:15:39 -0500

    Got github repos array working, nil pointer found

--- backend/graph/routes/Github.go
+++ backend/graph/routes/Github.go
@@ -7,7 +7,6 @@ import (
 	"github.com/google/go-github/github"
 	"github.com/rs/zerolog"
 
-	// "github.com/rs/zerolog/log"
 	"github.com/zenith110/portfilo/graph/model"
 )
 
@@ -20,8 +19,22 @@ func FetchProjects(githubUser string) (*model.GithubProjects, error) {
 	client := github.NewClient(nil)
 	ctx := context.Background()
 	repos, _, err := client.Repositories.List(ctx, githubUser, nil)
+	var projects []*model.Project
 	// name, HTMLURL, Description
-	fmt.Println(repos[0])
-	var githubproj model.GithubProjects
-	return &githubproj, err
+
+	// var githubproj model.GithubProjects{Projects}
+	for repo := range repos {
+		if repos[repo] != nil {
+			projects = append(projects, &model.Project{
+				Name:   *repos[repo].Name,
+				Link:   *repos[repo].HTMLURL,
+				Readme: *repos[repo].Description,
+			})
+		} else {
+			fmt.Print("Found a nil!")
+		}
+	}
+	fmt.Print("projects is: ", projects)
+	var gitProjects = model.GithubProjects{Projects: projects}
+	return &gitProjects, err
 }
